efe7faee3fa6d47d0fa7f1d19ec976836e09a20a
Harshit Jain <harshitjain0502@gmail.com>
2025-02-03 10:41:57 +0530

    LA3: add checks

--- LA3/psycopg2/run.sh
+++ LA3/psycopg2/run.sh
@@ -1,2 +1,10 @@
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+    source ./venv/bin/activate
+    pip install --upgrade pip
+    pip install psycopg2-binary python-dotenv
+    deactivate
+fi
+
 source ./venv/bin/activate
 python3 queries.py
